0177a0e6da0e88c369b8ce290b67452759efc1b4
ansakhalid <65833809+ansakhalid@users.noreply.github.com>
2020-07-19 23:03:44 +0500

    designer check

--- final-year-project/backend/controllers/userprofiling.js
+++ final-year-project/backend/controllers/userprofiling.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
-const jwt = require('jsonwebtoken'); // to generate signed token
-const expressJwt = require('express-jwt'); // for authorization check
+const jwt = require('jsonwebtoken'); 
+const expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
@@ -18,28 +18,9 @@ exports.signup = (req, res) => {
   });
 };
 
-// using async/await
-// exports.signup = async (req, res) => {
-//     try {
-//         const user = await new User(req.body);
-//         console.log(req.body);
 
-//         await user.save((err, user) => {
-//             if (err) {
-//                 // return res.status(400).json({ err });
-//                 return res.status(400).json({
-//                     error: 'Email is taken'
-//                 });
-//             }
-//             res.status(200).json({ user });
-//         });
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// };
 
 exports.signin = (req, res) => {
-  // find the user based on email
   const { email, password } = req.body;
   User.findOne({ email }, (err, user) => {
     if (err || !user) {
@@ -47,18 +28,13 @@ exports.signin = (req, res) => {
         error: 'User with that email does not exist. Please signup',
       });
     }
-    // if user is found make sure the email and password match
-    // create authenticate method in user model
     if (!user.authenticate(password)) {
       return res.status(401).json({
         error: 'Email and password dont match',
       });
     }
-    // generate a signed token with user id and secret
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-    // persist the token as 't' in cookie with expiry date
     res.cookie('t', token, { expire: new Date() + 9999 });
-    // return response with user and token to frontend client
     const { _id, name, email, role } = user;
     return res.json({ token, user: { _id, email, name, role } });
   });
@@ -71,11 +47,9 @@ exports.signout = (req, res) => {
 
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
-  userProperty: 'auth', //adding auth as user property
+  userProperty: 'auth', 
 });
 
-//loggedin and authenticated user should be same id
-//user cannot access someone elses account after proving only their id
 exports.isAuth = (req, res, next) => {
   let user = req.profile && req.auth && req.profile._id == req.auth._id;
   if (!user) {
